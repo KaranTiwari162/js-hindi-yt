16acbf28ce17c884af9d462edd15b0cef265d1bb
Karan Tiwari <webdevkaran18@gmail.com>
2026-04-12 14:35:50 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1445533
+let accountEmail = "tiwariji123@goggle.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+// accountId = 554553
+console.log(accountId);
+
+accountEmail = "kjrgghuoeyo@gn.com"
+accountPasswordc = "hdgohoee"
+accountCity = "sgsger"
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
+
